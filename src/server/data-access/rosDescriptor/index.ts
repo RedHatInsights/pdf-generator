280eca6445178bc1fbddca04f3abdaf09354ddb2
Casey Williams <caswilli@redhat.com>
2023-05-02 15:34:06 -0400

    Security-Compliance Rollback

--- src/server/data-access/rosDescriptor/index.ts
+++ src/server/data-access/rosDescriptor/index.ts
@@ -1,16 +1,14 @@
 import { ServiceCallFunction, ServiceDescriptor } from '../call-service';
 import ServiceNames from '../../../common/service-names';
 import config from '../../../common/config';
-import { rosExecutiveData, rosSystemFilters, rosSystemsData } from './rosData';
+import rosData from './rosData';
 import axios, { AxiosRequestHeaders } from 'axios';
-import QueryString from 'qs';
 
 const BASE_URL = `http://${config?.endpoints['ros-backend']?.hostname}:${config?.endpoints['ros-backend']?.port}/api/ros/v1`;
 const EXECUTIVE_REPORT_URL = `${BASE_URL}/executive_report`;
-const SYSTEMS_URL = `${BASE_URL}/systems`;
 
 const getExecutiveReport = async (headers: AxiosRequestHeaders) => {
-  const { data } = await axios.get<{ data: typeof rosExecutiveData }>(
+  const { data } = await axios.get<{ data: typeof rosData }>(
     EXECUTIVE_REPORT_URL,
     {
       headers,
@@ -19,51 +17,17 @@ const getExecutiveReport = async (headers: AxiosRequestHeaders) => {
   return { data };
 };
 
-const getSystemsReport = async (
-  headers: AxiosRequestHeaders,
-  { params }: Record<string, any>
-) => {
-  const defaultParams = {
-    order_by: 'report_date',
-    order_how: 'desc',
-    limit: -1,
-    ...params,
-  };
-
-  const data = await axios.get(SYSTEMS_URL, {
-    headers,
-    params: defaultParams,
-    paramsSerializer: (params) =>
-      QueryString.stringify(params, { arrayFormat: 'repeat' }),
-  });
-
-  return { data, filters: params };
-};
-
-const executiveGetMock: ServiceCallFunction = () =>
-  Promise.resolve({ data: rosExecutiveData });
-const executiveResponseProcessor = (data: typeof rosExecutiveData) => data;
-
-const systemsGetMock: ServiceCallFunction = () =>
-  Promise.resolve({ data: rosSystemsData, filters: rosSystemFilters });
-
-const systemsResponseProcessor = (data: typeof rosSystemsData) => data;
+const getMock: ServiceCallFunction = () => Promise.resolve({ data: rosData });
+const responseProcessor = (data: typeof rosData) => data;
 
 const rosDescriptor: ServiceDescriptor = {
   templates: {
     executiveReport: {
       service: ServiceNames.ros,
-      responseProcessor: executiveResponseProcessor,
+      responseProcessor,
       path: '/',
       request: getExecutiveReport,
-      mock: executiveGetMock,
-    },
-    systemsReport: {
-      service: ServiceNames.ros,
-      responseProcessor: systemsResponseProcessor,
-      path: '/',
-      request: getSystemsReport,
-      mock: systemsGetMock,
+      mock: getMock,
     },
   },
 };
