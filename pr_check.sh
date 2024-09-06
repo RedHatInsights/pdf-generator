4312423730bb7dabbf06c7fb779e4c58a73f4aa5
Blake Holifield <bholifie@redhat.com>
2024-09-06 10:12:07 -0400

    don't remove components resources PR check var

--- pr_check.sh
+++ pr_check.sh
@@ -11,6 +11,7 @@ export APP_ROOT=$(pwd)
 # Don't deploy all the dependencies
 export OPTIONAL_DEPS_METHOD="none"
 export DEPLOY_TIMEOUT="1200"
+export COMPONENTS_W_RESOURCES="crc-pdf-generator"
 
 # IQE_PLUGINS="e2e"
 # IQE_MARKER_EXPRESSION="smoke"
