1ed433a5f6b1823fc37122fbb5f9cf0253c91feb
Justin Orringer <justinorringer@gmail.com>
2025-02-18 11:58:32 -0500

    Adding advisor-backend to ServiceEndpoints

--- src/integration/endpoints.ts
+++ src/integration/endpoints.ts
@@ -3,6 +3,7 @@ import { Endpoint } from 'app-common-js';
 export enum ServiceNames {
   'ros-backend' = 'ros-backend',
   'chrome-service' = 'chrome-service',
+  'advisor-backend' = 'advisor-backend',
 }
 
 export type ServicesEndpoints = {
