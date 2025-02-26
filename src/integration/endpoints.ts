8aa2e6b033d12e85f2c5ac31c852895b8404247b
justinorringer <justinorringer@gmail.com>
2025-02-26 13:03:42 -0500

    Changing to advisor service name

--- src/integration/endpoints.ts
+++ src/integration/endpoints.ts
@@ -3,7 +3,7 @@ import { Endpoint } from 'app-common-js';
 export enum ServiceNames {
   'ros-backend' = 'ros-backend',
   'chrome-service' = 'chrome-service',
-  'advisor-backend' = 'advisor',
+  'advisor-backend' = 'advisor-backend',
   'vulnerability-engine' = 'vulnerability-engine',
 }
 
