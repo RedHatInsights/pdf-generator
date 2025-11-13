5e718fe8dc67040eb5880f40e8bd4bb2723cab51
Martin Marosi <marvusm.mmi@gmail.com>
2025-11-13 09:56:55 +0100

    fix: include pf-6-styles

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,5 @@
+import 'pf-5-styles/patternfly.min.css';
+import 'pf-5-styles/patternfly-addons.css';
 import '@patternfly/patternfly/patternfly.min.css';
 import '@patternfly/patternfly/patternfly-addons.css';
 import('./bootstrap');
